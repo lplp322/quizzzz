2664319df2744eb4dc08503d35d41622ed0cb924
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-02-25 21:08:59 +0100

    JavaDocs added and PUT modernised

--- server/src/main/java/server/SomeController.java
+++ server/src/main/java/server/SomeController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class SomeController {
     private int gameId = 0;
-    //create new Game object
+
+    /**
+     * Answering to the first client response, when new user is connecting to the game
+     * @return gameID - game ID of this player
+     */
     @GetMapping("/")
     @ResponseBody
     public String newPlayer() {
@@ -16,6 +20,12 @@ public class SomeController {
         return "Your game id is: "+gameId;
     }
 
+    /**
+     * Posting the new score of this player
+     * @param id - id of player
+     * @param score - score is received from player and posted to database/player object
+     * @return response from the server
+     */
     @PostMapping("/playerScore/{id}")
     @ResponseBody
     public String postResult(@PathVariable int id, @RequestBody String score){
@@ -23,13 +33,25 @@ public class SomeController {
         return "200 OK";
     }
 
+    /**
+     * Putting answer to the provided question of player with this id
+     * @param id - id of this player
+     * @param answer - answer(probably will be boolean or number)
+     * @return response from the server
+     */
     @PutMapping("/newAnswer/{id}")
     @ResponseBody
     public String putAnswer(@PathVariable int id, @RequestBody String answer){
         System.out.println("Player with name "+ id + " has answered: " + answer);
-        return "200 OK";
+        boolean isCorrect = true; //hardcoded for now
+        return "200 OK /n You answered: " + answer + " isCorrect: " + isCorrect;
     }
-    
+
+    /**
+     * Deleting player with provided ID, if the player left game
+     * @param id - id of this player
+     * @return
+     */
     @DeleteMapping("deletePlayer/{id}")
     @ResponseBody
     public String deletePlayer(@PathVariable int id){
