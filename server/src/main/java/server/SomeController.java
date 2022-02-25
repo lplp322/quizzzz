19bc8acabcc2ec327883b3772f9600267edbb8af
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-02-25 19:56:04 +0100

    Added (GET/PUT/POST/DELETE) basic controller

--- server/src/main/java/server/SomeController.java
+++ server/src/main/java/server/SomeController.java
@@ -1,17 +1,39 @@
 package server;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
 public class SomeController {
-
+    private int gameId = 0;
+    //create new Game object
     @GetMapping("/")
     @ResponseBody
-    public String index() {
-        return "Hello world!";
+    public String newPlayer() {
+        gameId++;
+        //if(currentGame.status()==true, then create new game with next gameID
+        return "Your game id is: "+gameId;
+    }
+
+    @PostMapping("/playerScore/{id}")
+    @ResponseBody
+    public String postResult(@PathVariable int id, @RequestBody String score){
+        System.out.println("Player with name "+ id + " has received: " + score);
+        return "200 OK";
+    }
+
+    @PutMapping("/newAnswer/{id}")
+    @ResponseBody
+    public String putAnswer(@PathVariable int id, @RequestBody String answer){
+        System.out.println("Player with name "+ id + " has answered: " + answer);
+        return "200 OK";
+    }
+    
+    @DeleteMapping("deletePlayer/{id}")
+    @ResponseBody
+    public String deletePlayer(@PathVariable int id){
+        System.out.println("Player with name: " + id + " left the game and was deleted");
+        return "200 OK";
     }
 }
