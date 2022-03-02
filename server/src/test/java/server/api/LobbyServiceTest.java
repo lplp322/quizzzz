407180982bd079f13d86c4dc99a59569937b5b0f
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-03-02 13:46:50 +0100

    CheckStyle issues fixed

--- server/src/test/java/server/api/LobbyServiceTest.java
+++ server/src/test/java/server/api/LobbyServiceTest.java
@@ -4,18 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import server.Activity;
 import server.LobbyService;
 import server.database.ActivityRepository;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
