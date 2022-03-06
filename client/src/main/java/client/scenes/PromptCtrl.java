4f3ac6cb07f104fc467fd38cef4a5cb6fcb52e3b
Stefan Stoicescu <Stoicescu@student.tudelft.nl>
2022-03-06 20:55:44 +0100

    Basic functionality for buttons

--- client/src/main/java/client/scenes/PromptCtrl.java
+++ client/src/main/java/client/scenes/PromptCtrl.java
@@ -1,12 +1,28 @@
 package client.scenes;
 
+import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class PromptCtrl {
+    private final MainCtrl mainCtrl;
     @FXML
     private AnchorPane mainWindow;
-
+    @FXML
+    private Button menuButton;
+    @FXML
+    private Button startButton;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Label errorLabel;
+    @Inject
+    public PromptCtrl(MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+    }
     /**
      * Changes the size of the AnchorPlane
      * @param w - preferred width
@@ -15,5 +31,19 @@ public class PromptCtrl {
     @FXML
     public void setWindowSize(double w, double h){
         mainWindow.setPrefSize(w,h);
+    }
+
+    @FXML
+    public void onClickStart(){
+        if(nameField.getText().matches("[a-zA-Z0-9]+")){
+            errorLabel.setVisible(false);
+        }
+        else{
+            errorLabel.setVisible(true);
+            errorLabel.setText("Name can only contain letters/numbers!");
+        }
+    }
+    public void onClickMenu(){
+        this.mainCtrl.showSplash();
     }
 }
