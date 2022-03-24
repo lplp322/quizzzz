89dbb30a92a2cdf70bd89b85697922ebae5aaeea
Borislav <B.N.Semerdzhiev@student.tudelft.nl>
2022-03-24 16:17:00 +0200

    fixed checkstyle

--- client/src/main/java/client/scenes/LobbyCtrl.java
+++ client/src/main/java/client/scenes/LobbyCtrl.java
@@ -7,12 +7,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class LobbyCtrl {
     private MainCtrl mainCtrl;
@@ -179,7 +177,7 @@ public class LobbyCtrl {
             http.disconnect();
         }
         catch (IOException e) {
-
+            e.printStackTrace();
         }
     }
 }
