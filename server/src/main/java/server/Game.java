f1d5d7557cb79b386ee4b04fa0b0339833bfdc60
Christopher Charlesworth <c.charlesworth@student.tudelft.nl>
2022-03-25 22:56:02 +0100

    checkstyle

--- server/src/main/java/server/Game.java
+++ server/src/main/java/server/Game.java
@@ -4,7 +4,12 @@ import commons.LeaderboardEntry;
 import commons.TrimmedGame;
 import server.database.ActivityRepository;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 public class Game implements Runnable{
     private Map<String, Player> players;
