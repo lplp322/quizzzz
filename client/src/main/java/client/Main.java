2a5736a8225876e74b34bdd044afe4e4f9a9c277
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-03-28 16:06:28 +0200

    Checkstyle fixed

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -71,7 +71,8 @@ public class Main extends Application {
 
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
 
-        mainCtrl.initialize(primaryStage, overview, add, splash, game, prompt, leaderboard, chooseServer, lobby, activityViewer);
+        mainCtrl.initialize(primaryStage, overview, add, splash, game, prompt, leaderboard,
+                chooseServer, lobby, activityViewer);
         
     }
 }
